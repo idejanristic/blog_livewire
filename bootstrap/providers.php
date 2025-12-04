e6db362167bcdedc83ae656cdcb3d47d1df00922
dejan <dejanr77@yahoo.com>
2025-12-04 14:05:20 +0100

    added 'barryvdh/laravel-debugbar' only on local environment

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,14 @@
 <?php
 
-return [
+$isLocal = env('APP_ENV') === 'local';
+
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
 ];
+
+if ($isLocal) {
+    $providers[] = Barryvdh\Debugbar\ServiceProvider::class;
+}
+
+return $providers;
